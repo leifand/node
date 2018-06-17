50b3f263b7d64ba55d0b67dd801623f4b0c2668a
Leif Anderson <leif.v.anderson@gmail.com>
2018-06-17 00:57:29 -0500

    update

--- stream/example2.js
+++ stream/example2.js
@@ -7,17 +7,7 @@ http.createServer(function (req, res) {
     body += chunk;
     console.log(chunk);
   });
-  /*req.on('end', () => {
-    try {
-      const data = JSON.parse(body);
-      res.write(typeof data);
-      res.end();
-    } catch (er) {
-      res.statusCode = 400;
-      return res.end(`error: ${er.message}`);
-    }
-  });*/
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('ANONYMOUS COMMENT RECEIVED\n');
+  res.end('REQUEST RECEIVED\n');
 }).listen(1336, 'localhost');
-console.log('ANONYMOUS COMMENT SERVER RUNNING ON PORT 1336');
+console.log('SERVER RUNNING ON PORT 1336');
