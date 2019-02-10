9e6913a4f960a13cf2d344590a2fa31f0ac1c86d
Leif Anderson <leif.v.anderson@gmail.com>
2019-02-09 20:27:21 -0600

    example scraper

---
+++ scrape/index.js
@@ -0,0 +1,17 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+let get_data = html => {
+    data = [];
+    const $ = cheerio.load(html);
+    $('table.itemlist tr td:nth-child(3)').each((i,elem)=>{
+        data.push({
+            title:$(elem).text(),
+            link:$(elem).find('a.storylink').attr('href')
+        });
+    });
+    console.log(data);
+}
+
+const url = 'https://news.ycombinator.com';
+axios.get(url).then(response=>{get_data(response.data);}).catch(error=>{console.log(error);})
