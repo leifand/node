531f431d4c3e821768144ce24bed49b88da55e65
Leif Anderson <leif.v.anderson@gmail.com>
2019-03-13 13:38:37 -0500

    naf

--- scrape/Scraper.js
+++ scrape/Scraper.js
@@ -1,6 +1,21 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
 
+class Scraper {
+    
+    /*state = {
+        ERROR : new Symbol('error'),
+        WAITING : new Symbol('waiting'),
+        SUCCESS : new Symbol('success')
+    };*/
+    
+    constructor() {
+        this.sym = new Symbol();
+        this.data = [];
+        this.url = null;
+    }
+}
+
 let get_data = html => {
     data = [];
     const $ = cheerio.load(html);
@@ -12,7 +27,6 @@ let get_data = html => {
     });
     console.log(data);
 }
-
 
 const url = 'https://news.ycombinator.com';
 axios.get(url).then(response=>{get_data(response.data);}).catch(error=>{console.log(error);})
