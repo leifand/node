e08637a476143dd0155591e1a4d510d448d43c49
Leif Anderson <leif.v.anderson@gmail.com>
2019-04-23 05:11:17 -0500

    found a straggler

---
+++ asyncfun.ts
@@ -0,0 +1,24 @@
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+
+async function longProcess() {
+  const { stdout, stderr } = await exec('ps aux | grep ssh');
+  console.log('stdout:', stdout);
+  console.log('stderr:', stderr);
+}
+
+async function shortProcess() {
+  const { stdout, stderr } = await exec('ls');
+  console.log('stdout:', stdout);
+  console.log('stderr:', stderr);
+}
+
+async function fastProcess() { 
+  const { stdout, stderr } = await exec('echo \'That was fast!\'');
+  console.log('stdout:', stdout);
+  console.log('stderr:', stderr);
+}
+
+longProcess();
+shortProcess();
+fastProcess();
